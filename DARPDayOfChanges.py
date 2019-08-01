728a85092a0f42681bf6dd842df68e4015025da9
tfos11 <37597194+tfos11@users.noreply.github.com>
2019-08-01 15:27:51 -0300

    Update and rename DARPDayOfv2.py to DARPDayOfChanges.py

--- DARPDayOfChanges.py
+++ DARPDayOfChanges.py
@@ -1,17 +1,20 @@
+#Import pyomo libraries
+
 from pyomo.environ import *
 import pyomo.opt
 infinity = float('inf')
 
-#Declare model
+#Declare model type
 model = AbstractModel()
 
-path = "C:\\Users\\tr930605\\DARP Model\\DARP Input Files\\"
+#Set path to input files
+path = "C:\\Users\\tfos11\\DARP Model\\DARP Input Files\\"
 
 
-#Declare number of requests and number of PTUs
-model.n = Param(initialize = 57)
-model.PTU = Param(initialize = 7)
-model.M = Param(initialize = 10000)
+#Declare number of requests and number of PTUs and the Big M constant
+model.n = Param(initialize = )
+model.PTU = Param(initialize = )
+model.M = Param(initialize = )
 
 #Declare new global parameters
 model.G = Param(initialize = 100)
@@ -22,7 +25,7 @@ model.alphaminus = Param(initialize = 5)
 model.Slow = Param(initialize = 0)
 model.Shigh = Param(initialize = 900)
 
-#Declare the proper indices
+#Declare the node and PTU indices
 model.v = RangeSet(0, 2*model.n+1)
 model.i = RangeSet(0, 2*model.n+1, within=model.v)
 model.j = RangeSet(0, 2*model.n+1, within=model.v)
@@ -34,36 +37,38 @@ model.d = RangeSet(model.n+1, 2*model.n, within=model.v)
 model.pd = RangeSet(1, 2*model.n, within=model.v)
 model.dep = RangeSet(0, 2*model.n+1, 2*model.n+1, within=model.v)
 
-#Declare decision variables
+#Declare route, service time and vehicle load decision variables
 model.x = Var(model.i, model.j, model.k, domain = Binary)
 model.service = Var(model.v, model.k, domain = NonNegativeReals)
 model.ld = Var(model.v, model.k, domain = NonNegativeReals)
 
+#Declare changed pickup time decision variables
 model.Y = Var(model.i, domain = Binary)
 model.Z = Var(model.i, domain = Binary)
 
+#Declare service time dummy variable and pickup time difference decision variable
 model.s = Var(model.i, model.j, model.k, domain = NonNegativeReals)
 model.b = Var(model.i, domain = Reals)
-#Declare single indexed paramters
+
+#Declare single indexed paramters: service time, time windows, load and previous pickup time
 
 model.di = Param(model.i)
 model.ei = Param(model.i)
 model.li = Param(model.i)
 
-#Declare previous arrival times
 model.wi = Param(model.p)
 
 model.qi = Param(model.i)
 
-model.MaxTime = Param(model.k, initialize={1:720, 2:690, 3:900, 4:540, 5:750, 6:600, 7:780})
-model.MinTime = Param(model.k, initialize={1:0, 2:60, 3:30, 4:30, 5:90, 6:60, 7:60})
-model.cap = Param(model.k, initialize={1:1, 2:1, 3:1, 4:1, 5:1, 6:1, 7:1})
+#Declare vehicle shift start and end times and capacity
+model.MaxTime = Param(model.k, initialize={1:900})
+model.MinTime = Param(model.k, initialize={1:0})
+model.cap = Param(model.k, initialize={1:1})
 
-#Declare double indexed paramters
+#Declare double indexed paramters:travel time and route cost
 model.t = Param(model.i, model.j)
 
-#Declare cost paramters
-#model.c = Param(model.i, model.j)
+model.c = Param(model.i, model.j)
 
 #Load single index data
 
@@ -86,20 +91,22 @@ data.load(filename = path +"Tij.csv", param = model.t)
 
 #data.load(filename = path +"Tij.csv", param = model.c)
 
-"Delcar workload balancing parameter"
+#Declare workload balancing parameters
 
-model.wb2 = Param(initialize = 900)
-model.wb1 = Param(initialize = 25)
+model.wb2 = Param(initialize = )
+model.wb1 = Param(initialize = )
 
+#This is the objective function for the general DARP model
 #def ObjRule(model):
 #    return sum(model.c[i,j]*model.x[i,j,k] for i in model.i for j in model.j for k in model.k)
 #model.Obj = Objective(rule=ObjRule, sense=minimize)
 
+#Declare the new objective function: Minimize the number of changed pickup times
 def ObjRule(model):
     return sum(model.Y[i] for i in model.p) + sum(model.Z[i] for i in model.p)
 model.Obj = Objective(rule=ObjRule, sense=minimize)
 
-"Routing Constraints"
+#Constraints A through F are the routing constraints
 
 def A_rule(model, i):
     return sum(model.x[i, j, k] for j in model.j for k in model.k) == 1 
@@ -121,15 +128,11 @@ def E_rule(model, i, k):
     return sum(model.x[j, i, k] for j in model.j) - sum(model.x[i, j, k] for j in model.j) == 0 
 model.c_e = Constraint(model.pd, model.k, rule=E_rule)
 
-def N_rule(model, i, j, k):
-    return model.x[i, i, k] == 0
-model.c_n = Constraint(model.i, model.j, model.k, rule=N_rule)
-
-"Service Constraints"
-
 def F_rule(model, i, j, k):
-    return model.service[j, k] >= model.service[i,k] + model.di[i] + model.t[i,j] - model.M*(1-model.x[i, j, k])
+    return model.x[i, i, k] == 0
 model.c_f = Constraint(model.i, model.j, model.k, rule=F_rule)
+
+#Constraints G through K are the service time constraints
 
 def G_rule(model, k):
     return model.service[2*model.n+1, k] <= model.MaxTime[k]
@@ -139,27 +142,19 @@ def H_rule(model, i, k):
     return model.ei[i] <= model.service[i, k] <= model.li[i]
 model.c_h = Constraint(model.i, model.k, rule=H_rule)
 
-def O_rule(model, k):
+def I_rule(model, k):
     return model.service[0, k] >= model.MinTime[k]
-model.c_o = Constraint(model.k, rule=O_rule)
+model.c_i = Constraint(model.k, rule=I_rule)
 
-def Q_rule(model, i, j, k):
+def J_rule(model, i, j, k):
     return model.service[i+model.n, k] >= model.service[i,k] + model.di[i] + model.t[i,j] - model.M*(1-model.x[i, j, k])
-model.q_f = Constraint(model.p, model.j, model.k, rule=Q_rule)
+model.q_j = Constraint(model.p, model.j, model.k, rule=J_rule)
 
-"Load Constraints"
+def K_rule(model, i, j, k):
+    return model.service[j, k] >= model.service[i,k] + model.di[i] + model.t[i,j] - model.M*(1-model.x[i, j, k])
+model.c_k = Constraint(model.i, model.j, model.k, rule=K_rule)
 
-def I_rule(model, i, j, k):
-    return model.ld[j, k] >= model.ld[i, k] + model.qi[j] - model.M*(1-model.x[i, j, k])
-model.c_i = Constraint(model.i, model.j, model.k, rule=I_rule)
-
-def J_rule(model, i, k):
-    return model.ld[i, k] <= model.cap[k]
-model.c_j = Constraint(model.i, model.k, rule=J_rule)
-
-def K_rule(model, i, k):
-    return model.ld[i, k] <= model.cap[k] + model.qi[i]
-model.c_k = Constraint(model.i, model.k, rule=K_rule)
+#Constraints L through Q are the load constraints
 
 def L_rule(model, i, k):
     return model.ld[i, k] >= 0
@@ -169,11 +164,23 @@ def M_rule(model, i, k):
     return model.ld[i, k] >= model.qi[i]
 model.c_m = Constraint(model.i, model.k, rule=M_rule) 
 
+def N_rule(model, i, j, k):
+    return model.ld[j, k] >= model.ld[i, k] + model.qi[j] - model.M*(1-model.x[i, j, k])
+model.c_n = Constraint(model.i, model.j, model.k, rule=N_rule)
+
+def O_rule(model, i, k):
+    return model.ld[i, k] <= model.cap[k]
+model.c_o = Constraint(model.i, model.k, rule=O_rule)
+
 def P_rule(model, i, j, k):
     return model.ld[j, k] <= model.ld[i, k] + model.qi[j] + model.M*(1-model.x[i, j, k])
 model.c_p = Constraint(model.i, model.j, model.k, rule=P_rule)
 
-"New Constraints"
+def Q_rule(model, i, k):
+    return model.ld[i, k] <= model.cap[k] + model.qi[i]
+model.c_q = Constraint(model.i, model.k, rule=Q_rule)
+
+#Constraints R through Z are the new constraints to count the number of changed pickup times
 
 def R_rule(model, i):
     return sum(model.s[i,j,k] for j in model.j for k in model.k) - sum(model.x[i,j,k]*model.wi[i] for j in model.j for k in model.k) == model.b[i]
@@ -211,7 +218,7 @@ def Z_rule(model, i):
         return -model.G*model.Z[i] <= model.b[i] + model.alphaminus
 model.c_z = Constraint(model.p, rule=Z_rule)
 
-"Declare workload balancing constraint 1: Each PTU workload must be less then some predetermined value"
+#Declare workload balancing constraintsL Each vehicle workload must be less then some predetermined value and greater than another
 
 def AA_rule (model, k):
     return sum((model.t[i,j] + model.di[i])*model.x[i,j,k] for i in model.i for j in model.j) <= model.wb2
@@ -221,73 +228,32 @@ def BB_rule (model, k):
     return sum((model.t[i,j] + model.di[i])*model.x[i,j,k] for i in model.i for j in model.j) >= model.wb1
 model.c_bb = Constraint(model.k, rule=BB_rule)
 
+#Create model instance
 instance = model.create_instance(data)
 
+#model.write("instance.lp")
+
+#Assign solver and set options if desired
 solver = pyomo.opt.SolverFactory('gurobi')
-solver.options["mipgap"]=0.05
+#solver.options["mipgap"]=0.02
 solver.options["timelimit"]=21600
 
+#Solve the DARP. Turn tee to False if you do not want to see the status of the solver
 results = solver.solve(instance, tee=True)
 
+#See the results
 results.write()
+
+#print the total driving time for comparison with the general DARP model
 print(sum(instance.t[i,j]*instance.x[i,j,k].value for i in instance.i for j in instance.j for k in instance.k))
 
-### Create the gurobi_ampl solver plugin using the ASL interface
-solver = 'gurobi_ampl'
-solver_io = 'nl'
-stream_solver = True     # True prints solver output to screen
-keepfiles =     False     # True prints intermediate file names (.nl,.sol,...)
-opt = SolverFactory(solver,solver_io=solver_io)
-
-if opt is None:
-    print("")
-    print("ERROR: Unable to create solver plugin for %s "\
-          "using the %s interface" % (solver, solver_io))
-    print("")
-    exit(1)
-
-# tell gurobi to be verbose with output
-opt.options['outlev'] = 1
-
-# tell gurobi to find an iis table for the infeasible model
-opt.options['iisfind'] = 1 # tell gurobi to be verbose with output
-
-# Create an IMPORT Suffix to store the iis information that will
-# be returned by gurobi_ampl
-instance.iis = Suffix(direction=Suffix.IMPORT)
-
-### Send the model to gurobi_ampl and collect the solution
-# The solver plugin will scan the model for all active suffixes
-# valid for importing, which it will store into the results object
-results = opt.solve(instance,
-                    keepfiles=keepfiles,
-                    tee=stream_solver)
-
-print("")
-print("IIS Results")
-for component, value in instance.iis.items():
-    print(component.name+" "+str(value))
-    
-#log_infeasible_constraints(instance)
-
-
-print(instance.service[27,1].value)
-
+#print total workload for each vehicle
 for k in instance.k:
     print(sum((instance.t[i,j] + instance.di[i])*instance.x[i,j,k].value for i in instance.i for j in instance.j))
 
-with open(path+'service.txt', 'w') as f:
-    for v in instance.v:
-        for k in instance.k:
-            print(v, k, instance.service[v,k].value, file=f)
-
+#print decision variables along with the node to a text file
 with open(path+'SameDayResults.txt', 'w') as f:
     for i in instance.i:
         for j in instance.j:
             for k in instance.k:
                 print(i, j, k, instance.x[i,j, k].value, instance.service[i,k].value, instance.ld[i,k].value, file=f)
-
-with open(path+'load.txt', 'w') as f:
-    for v in instance.v:
-        for k in instance.k:
-            print(v, k, instance.ld[v,k].value, file=f)
